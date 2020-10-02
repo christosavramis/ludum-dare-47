69da014473a6313a15e01b1e015c9ee91596b491
Christos Avramis <it174918@it.teithe.gr>
2020-10-02 22:42:57 +0300

    Initial Commit
    create MainMenu scene, Game scene and Ending scene

---
+++ Assets/Scripts/CustomSceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomSceneManager : MonoBehaviour
+{
+    public static void LoadScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public void LoadSceneInstance(int i)
+    {
+        CustomSceneManager.LoadScene(i);
+    }
+}
