275f14aeedf1f8dd5d79ed8e54098bb0e6366f37
Stefania Makrygiannaki <stefania-mak-@hotmail.com>
2020-10-05 02:05:19 +0300

    feat: mouse events basic methods work

--- Assets/NPCMovement.cs
+++ Assets/NPCMovement.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class NPCMovement : MonoBehaviour
+public class NPCMovement : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
 
     public float moveSpeed = 5f;
@@ -11,6 +12,8 @@ public class NPCMovement : MonoBehaviour
 
     Vector2 direction = -Vector2.right;
 
+   
+    
     void Start()
     {
         rb.velocity = direction * moveSpeed;
@@ -26,7 +29,38 @@ public class NPCMovement : MonoBehaviour
 
         if (transform.position.x < screenBottomLeft.x)
         {
-            rb.MovePosition(new Vector2(screenTopRight.x, transform.position.y));           
+            rb.MovePosition(new Vector2(screenTopRight.x, transform.position.y));
+            Debug.Log("Start again");
         }
     }
+
+    
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        //rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+    
 }
